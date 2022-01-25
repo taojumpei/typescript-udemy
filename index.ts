890af9f77190bc14fe20ef3e20c97cbf1d65f8b0
taojumpei <jum-tao@nec.com>
2022-01-25 20:51:52 +0900

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,38 @@
-// 定数
-const currentYear: number = 2020;
-// currentYear = 2021;
+// スプレッド演算子
+let categories = ['post', 'news', 'info'];
+// ...categories
 
-const PI = Math.PI;
+// 複製
+let copyCat = [...categories];
+// console.log(copyCat, copyCat === categories);
+
+// 連結
+let pushCat = ['other', ...categories];
+// console.log(pushCat);
+
+let unshiftCat = [...categories, 'other'];
+// console.log(unshiftCat);
+
+// 挿入
+let insertCat = ['other', ...categories, 'Q&A'];
+// console.log(insertCat);
+
+// マージ
+let mergeCat = [...categories, ...copyCat];
+// console.log(mergeCat);
+
+// オブジェクト
+let post = { id: 1, content: 'dummy', created: '2020-01-01' };
+
+let copyObj = { ...post };
+// console.log(copyObj);
+
+let pushObj = { ...post, updated: '2020-04-05' };
+// console.log(pushObj);
+
+let mergeObj = { ...post, pushObj };
+// console.log(mergeObj);
+
+let test = { ...categories };
+
+console.log(test);
